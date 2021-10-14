63a43109081e5a04b8b366a1b9919bd7fe07dc77
David Avery <eismausen@gmail.com>
2021-10-13 17:22:19 -0700

    commit tree 1ca82ea646ab54549013f4c4bc38b311502318dd

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
 // Code your solution here
+function findMatching(arrDriverNames, string) {
+    function filterFunc(element) {
+        return (element.toLowerCase() === string.toLowerCase())
+    }
+
+    let returnArr = arrDriverNames.filter(filterFunc);
+    return returnArr;
+}
+
+function fuzzyMatch(arrDriverNames, string) {
+    function partialMatch(element) {
+        return (element.slice(0, string.length) === string);
+    }
+    let returnArr = arrDriverNames.filter(partialMatch);
+    return returnArr;
+}
+
+function matchName(arrDrivers, string) {
+    function nameFilter(element) {
+        return (element.name.toLowerCase() === string.toLowerCase())
+    }
+
+    let returnArr = arrDrivers.filter(nameFilter);
+    return returnArr;
+}
